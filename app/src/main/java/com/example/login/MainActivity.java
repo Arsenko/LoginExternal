59776bfb8187850579371a889cdc05df57b1373a
Arsenko <minsenka@gmail.com>
2020-03-17 22:38:12 +0300

    finish

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -2,24 +2,31 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String LOGINS_FILE_NAME="logins.txt";
-    private final static String PASSWORDS_FILE_NAME="passwords.txt";
+    private final static String USERS_FILE_NAME="users.txt";
+    private final static String BOX_KEY="box";
+    SharedPreferences shared;
+    File source;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,46 +36,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init(){
+        source = new File(this.getExternalFilesDir(null), "source.txt");
+        shared=getPreferences(MODE_PRIVATE);
+
         final EditText login=findViewById(R.id.login);
         final EditText password=findViewById(R.id.password);
+        final CheckBox boxExternal=findViewById(R.id.boxExternal);
+
+        if(shared.contains(BOX_KEY)){
+            boxExternal.setChecked(shared.getBoolean(BOX_KEY,false));
+        }
+
+        boxExternal.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor ed=shared.edit();
+                ed.putBoolean(BOX_KEY,isChecked);
+                ed.commit();
+            }
+        });
 
         Button btnLogin=findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inName=login.getText().toString();
-                String inPassword=password.getText().toString();
-                try {
-                    FileInputStream streamInLogins = openFileInput(LOGINS_FILE_NAME);
-                    FileInputStream streamInPasswords = openFileInput(PASSWORDS_FILE_NAME);
-
-                    InputStreamReader ReadFromLogins = new InputStreamReader(streamInLogins);
-                    InputStreamReader ReadFromPasswords = new InputStreamReader(streamInPasswords);
-
-                    BufferedReader brLogins = new BufferedReader(ReadFromLogins);
-                    BufferedReader brPasswords = new BufferedReader(ReadFromPasswords);
-
-                    String log="";
-                    String pas="";
-                    boolean loginSuccess=false;
-                    while((log=brLogins.readLine())!=null && (pas=brPasswords.readLine())!=null) {
-                        Toast.makeText(getApplicationContext(),log+pas,Toast.LENGTH_LONG).show();
-                        if(log.equals(inName) && pas.equals(inPassword)){
-                            Toast.makeText(getApplicationContext(),getString(R.string.successLogin),Toast.LENGTH_LONG).show();
-                            loginSuccess=true;
-                            break;
-                        }
+                boolean loginSuccess=false;
+                String inName = login.getText().toString();
+                String inPassword = password.getText().toString();
+                if(boxExternal.isChecked()){
+                    try(FileReader fromSource = new FileReader(source)) {
+                        BufferedReader br = new BufferedReader(fromSource);
+                        loginSuccess=findUser(br,inName,inPassword);
+                    }catch (IOException e){
+                        Toast.makeText(getApplicationContext(), getString(R.string.fileExeption),
+                                Toast.LENGTH_LONG).show();
                     }
-                    brLogins.readLine();
-                    brPasswords.readLine();
-
-                    brLogins.close();
-                    brPasswords.close();
-                    if(!loginSuccess){
-                        Toast.makeText(getApplicationContext(),getString(R.string.loginPasswordNotFound),Toast.LENGTH_LONG).show();
+                }else {
+                    try {
+                        FileInputStream streamInUsers = openFileInput(USERS_FILE_NAME);
+                        InputStreamReader ReadFromUsers = new InputStreamReader(streamInUsers);
+                        BufferedReader brUsers = new BufferedReader(ReadFromUsers);
+                        loginSuccess=findUser(brUsers,inName,inPassword);
+                        brUsers.close();
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.fileExeption),
+                                Toast.LENGTH_LONG).show();
                     }
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(),getString(R.string.fileExeption),Toast.LENGTH_LONG).show();
+                }
+                if (!loginSuccess) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.loginPasswordNotFound),
+                            Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -76,29 +93,57 @@ public class MainActivity extends AppCompatActivity {
         btnRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String inName=login.getText().toString()+'\n';
-                String inPassword=password.getText().toString()+'\n';
-                try {
-                    FileOutputStream streamToLogins = openFileOutput(LOGINS_FILE_NAME, MODE_PRIVATE);
-                    FileOutputStream streamToPasswords = openFileOutput(PASSWORDS_FILE_NAME,MODE_PRIVATE);
+                String user = login.getText().toString() + '\n' + password.getText().toString();
+                if (boxExternal.isChecked()) {
+                    try (FileWriter toSource = new FileWriter(source, true)) {
+                        toSource.append(user);
+                        toSource.close();
+                        Toast.makeText(getApplicationContext(), getString(R.string.successFileWrite),
+                                Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(),getString(R.string.fileExeption),
+                                Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    try {
+                        FileOutputStream streamToLogins = openFileOutput(USERS_FILE_NAME, MODE_PRIVATE);
 
-                    OutputStreamWriter writeToLogins = new OutputStreamWriter(streamToLogins);
-                    OutputStreamWriter writeToPasswords = new OutputStreamWriter(streamToPasswords);
+                        OutputStreamWriter writeToUsers = new OutputStreamWriter(streamToLogins);
 
-                    BufferedWriter bwLogins = new BufferedWriter(writeToLogins);
-                    BufferedWriter bwPasswords = new BufferedWriter(writeToPasswords);
+                        BufferedWriter bwUsers = new BufferedWriter(writeToUsers);
 
-                    bwLogins.write(inName);
-                    bwPasswords.write(inPassword);
+                        bwUsers.write(user);
 
-                    bwLogins.close();
-                    bwPasswords.close();
+                        bwUsers.close();
 
-                    Toast.makeText(getApplicationContext(),getString(R.string.successFileWrite),Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(),getString(R.string.fileExeption),Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), getString(R.string.successFileWrite),
+                                Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.fileExeption),
+                                Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
+    }
+
+    private boolean findUser(BufferedReader brUsers,String inName,String inPassword){
+        String log = "";
+        String pas = "";
+        try {
+            while ((log = brUsers.readLine()) != null && (pas = brUsers.readLine()) != null) {
+                if (log.equals(inName) && pas.equals(inPassword)) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.successLogin),
+                            Toast.LENGTH_LONG).show();
+                    return true;
+                }
+            }
+            return false;
+        }catch (IOException e){
+            Toast.makeText(getApplicationContext(), getString(R.string.fileExeption),
+                    Toast.LENGTH_LONG).show();
+        }
+        return false;
+
     }
 }
